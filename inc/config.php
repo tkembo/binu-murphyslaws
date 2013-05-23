9e12b2e1f885a860b86f463eab2c5eb1e01f2d64
Tawanda Kembo <tkembo@gmail.com>
2013-05-23 11:07:19 +0200

    removed quotes from server variables

--- inc/config.php
+++ inc/config.php
@@ -1,9 +1,9 @@
 <?php
 	
-	$hostname_mxit_murphyslaws = "DB_HOST";
-	$database_mxit_murphyslaws = "DB_NAME";
-	$username_mxit_murphyslaws = "DB_USER";
-	$password_mxit_murphyslaws = "DB_PASS";
+	$hostname_mxit_murphyslaws = DB_HOST;
+	$database_mxit_murphyslaws = DB_NAME;
+	$username_mxit_murphyslaws = DB_USER;
+	$password_mxit_murphyslaws = DB_PASS;
 	
 	$mxit_murphyslaws = mysql_pconnect($hostname_mxit_murphyslaws, $username_mxit_murphyslaws, $password_mxit_murphyslaws) or trigger_error(mysql_error(),E_USER_ERROR); 
 	
